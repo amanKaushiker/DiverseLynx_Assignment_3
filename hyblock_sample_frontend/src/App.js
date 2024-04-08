beb99c8f15183b8325adc9bc44780de2fe4f057e
aman kaushik <amankaushiker@gmail.com>
2024-04-08 19:59:03 +0530

    frontent complete

--- hyblock_sample_frontend/src/App.js
+++ hyblock_sample_frontend/src/App.js
@@ -1,79 +1,69 @@
 import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { CategoryScale } from "chart.js";
-import { Bar } from "react-chartjs-2";
-
-// axios.create({
-//   baseURL: "http://localhost:4000",
-//   // You can add headers or other configurations here
-// });
+import LineChart from "./Components/LineChart";
+import { Chart } from "react-google-charts";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
 
 function App() {
-  const [data, setData] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        axios
-          .get("http://localhost:4000/getData/1m")
-          .then((data) => console.log(data.data))
-          .catch((error) => console.log(error));
-      } catch (error) {
-        console.error("Error fetching data: ", error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  const options = {
-    scales: {
-      x: {
-        type: "category",
-        labels: data.labels,
-      },
-      y: {
-        beginAtZero: true,
-      },
-    },
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  useEffect(() => {
-    setData({
-      labels: ["January", "February", "March", "April", "May"],
-      datasets: [
-        {
-          label: "Sales",
-          data: [65, 59, 80, 81, 56],
-          backgroundColor: "rgba(54, 162, 235, 0.2)",
-          borderColor: "rgba(54, 162, 235, 1)",
-          borderWidth: 1,
-        },
-      ],
-    });
-  }, []);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleData = (value) => {
+    if (
+      value === "1m" ||
+      value === "5m" ||
+      value === "15m" ||
+      value === "1h" ||
+      value === "4h" ||
+      value === "1d"
+    ) {
+      console.log("option : ", value);
+    }
+  };
 
   return (
     <div className="App">
+      <h1>Assignment 3</h1>
       <div>
-        <h2>Bar Chart Example</h2>
-        <Bar
-          data={{
-            labels: ["January", "February", "March", "April", "May"],
-            datasets: [
-              {
-                label: "Sales",
-                data: [65, 59, 80, 81, 56],
-                backgroundColor: "rgba(54, 162, 235, 0.2)",
-                borderColor: "rgba(54, 162, 235, 1)",
-                borderWidth: 1,
-              },
-            ],
+        <Button
+          onClick={handleClick}
+          style={{
+            backgroundColor: "blue",
+            color: "white",
+            padding: "10px 20px",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+            fontSize: "16px",
+            fontWeight: "bold",
           }}
-          options={options}
-        />
+        >
+          Time Slots
+        </Button>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={() => handleData("1m")}>1 min</MenuItem>
+          <MenuItem onClick={() => handleData("5m")}>5 min</MenuItem>
+          <MenuItem onClick={() => handleData("15m")}>15 min</MenuItem>
+          <MenuItem onClick={() => handleData("1h")}>1 hour</MenuItem>
+          <MenuItem onClick={() => handleData("4h")}>4 hour</MenuItem>
+          <MenuItem onClick={() => handleData("1d")}>1 Day</MenuItem>
+        </Menu>
       </div>
+      <LineChart />
     </div>
   );
 }
